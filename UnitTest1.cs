ece2b5410a066791d97f564722839afdd1f49d98
unknown <mayank.k.gupta89@gmail.com>
2025-08-14 19:47:25 +0530

    First Commit

---
+++ UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Playwright;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+namespace PlaywrightTests
+{
+    public class Tests
+    {
+        [Test]
+        public async Task GoogleTitleShouldContainGoogle()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = true
+            });
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://www.google.com");
+            var title = await page.TitleAsync();
+            Assert.That(title, Does.Contain("Google"));
+        }
+    }
+}
